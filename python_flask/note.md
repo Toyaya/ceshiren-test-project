37d976a250e720487ed53f187654232eb5db37b5
xuxin.ma <xuxin.ma@mihoyo.com>
2021-07-11 16:08:22 +0800

    添加flask代码

--- python_flask/note.md
+++ python_flask/note.md
@@ -1,1 +1,9 @@
 ##### app.run(debug=True,port=8080)
+
+#### flask resful：直接使用路由装饰器来定义接口看起来比较繁琐，整个接口代码会显得条理不清晰，混乱。restful能很好的管理接口。restful能很好的管理接口
+```
+def get()#使用什么方法。
+
+```
+####方法名对应app.route中的methods
+####https://ceshiren.com/t/topic/13462
---
+++ python_flask/server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+app = Flask(__name__)
+api = Api(app)
+
+
+# 类代表是哪个接口资源，每个方法，代表对此资源的操作，比如 get、post等
+# 在类服务中继承resource，表示使用flask-restful
+class TestCaseService(Resource):
+    """
+    测试用例服务
+    """
+    # 方法名，对应 app.route中的methods
+    def get(self):
+        # flask 内部自带的日志打印
+        app.logger.warning("get success")
+        app.logger.info("get success")
+        return {"error": 0, "msg": "get success"}
+
+    def post(self):
+        return {"error": 0, "msg": "post success"}
+
+    def put(self):
+        return {"error": 0, "msg": "put success"}
+
+    def delete(self):
+        return {"error": 0, "msg": "delete success"}
+
+class TaskService(Resource):
+    pass
+
+
+
+if __name__ == '__main__':
+    # 把服务添加到app flask 中
+    # 第一个参数是添加的接口服务， 第二个参数，是指定对应接口服务使用的路由
+    api.add_resource(TestCaseService, "/testcase")
+    api.add_resource(TaskService, "/task")
+    app.run(debug=True)
